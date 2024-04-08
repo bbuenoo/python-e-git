e4aff23629947d742de7c49aa92084752d069faa
Breno <brenofonseca419@gmail.com>
2024-04-08 18:17:59 -0300

    Meu primeiro programa

---
+++ app.py
@@ -0,0 +1,11 @@
+print('************************************')
+print('* ADIVINHE A SENHA -  GANHE PREMIOS *')
+print('************************************')
+print('* Digite a senha:                   ')
+senha = input()
+print('************************************')
+
+if senha== '1234' :
+    print('* Você digitou:', senha, '- VOCÊ ACERTOU *')
+print('************************************')
+print('A senha está incorreta!:')
